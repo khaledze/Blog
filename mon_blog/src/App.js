756ad92c3798b6516c4f8129179e390c2085956f
khaledze <k.aliahmed@ecole-ipssi.net>
2023-11-15 10:48:24 +0100

    changement

--- mon_blog/src/App.js
+++ mon_blog/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ConnexionPage from './components/ConnexionPage';
+import Home from './components/Home';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <ConnexionPage />
+      <Home />
     </div>
   );
 }
