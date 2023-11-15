944c7306b0cfa71ac7f6d43c062cfe6b57127aa3
lenzees <tcely2003@gmail.com>
2023-11-15 16:02:07 +0100

    home

--- mon_blog/src/components/Home.jsx
+++ mon_blog/src/components/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+
 import './Home.css';
 import headerImage from '../img/LOeil-sur-lecran.png';
 import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
+    const [articles, setArticles] = useState([]);
+  
+    useEffect(() => {
+      // Effectuer une requête à votre nouvelle route pour obtenir les détails des articles
+      fetch("http://localhost:3001/articles-details")
+        .then(response => response.json())
+        .then(data => setArticles(data))
+        .catch(error => console.error("Erreur lors de la récupération des articles :", error));
+    }, []); // Utilisation de [] comme dépendance pour que cet effet soit exécuté une seule fois
+  
   return (
     <div className="home-container">
       <header className="header">
@@ -19,7 +30,20 @@ export default function Home() {
           <Link to="/deconnexion"><button className="logout-button">Se déconnecter</button></Link>
         </div>
       </header>
-      <div>Home</div>
+      <div className="articles-container">
+        <h2>Derniers articles</h2>
+        <ul>
+          {articles.map(article => (
+            <li key={article.titre}>
+              <div className="article-details">
+                <h3>{article.titre}</h3>
+                <p>{article.contenu}</p>
+                <p>Date de création : {article.date_creation}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
